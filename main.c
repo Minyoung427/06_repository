0546b0db40721acc1f64faee6e2c3352c6342265
Minyoung427 <55172353+Minyoung427@users.noreply.github.com>
2019-10-10 16:01:07 +0900

    second

--- main.c
+++ main.c
@@ -2,20 +2,43 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int sumTwo(int a, int b){
+	int output;
+	output=a+b;
+	return output;
+	
+}
+
+int squre(int n){
+	return(n*n);
+}
+
+int get_max(int x, int y){
+	if(x>y) return(x);
+	else return(y);
+	}
+
+
 
 int main(int argc, char *argv[]) {
-
-void print_star()
-{
-	int i;
-	for(i=0;i<10;i++)
-		printf("*");
-}
-	int i;
+	int a,b;
+	int output;
 	
-	print_star();
-	print_star();
-	print_star();
-		
-	return 0;
+	a=2;
+	b=3;
+	output = sumTwo(a,b);
+	printf("sumTwo:%d\n", output);
+	
+	int n;
+	n=5;
+	squre(n);
+	printf("squre:%d\n", squre(n));
+	
+	int x,y;
+	x=2;
+	y=7;
+	
+	printf("max:%d\n",get_max(x,y));
+	
+
 }
