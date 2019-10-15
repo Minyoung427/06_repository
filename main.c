87d7c1ef0ed3df7947f8d9009db98537c8aebdb8
Minyoung427 <55172353+Minyoung427@users.noreply.github.com>
2019-10-15 15:24:36 +0900

    third

--- main.c
+++ main.c
@@ -2,43 +2,16 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int sumTwo(int a, int b){
-	int output;
-	output=a+b;
-	return output;
-	
-}
-
-int squre(int n){
-	return(n*n);
-}
-
-int get_max(int x, int y){
-	if(x>y) return(x);
-	else return(y);
-	}
+	int square(int n);
 
 
 
 int main(int argc, char *argv[]) {
-	int a,b;
-	int output;
-	
-	a=2;
-	b=3;
-	output = sumTwo(a,b);
-	printf("sumTwo:%d\n", output);
-	
-	int n;
-	n=5;
-	squre(n);
-	printf("squre:%d\n", squre(n));
-	
-	int x,y;
-	x=2;
-	y=7;
-	
-	printf("max:%d\n",get_max(x,y));
-	
-
+	int result;
+	result=square(5);
+	printf("%d",result);
 }
+	int square(int n)
+	{
+	return(n*n);
+		}
